6d78092acbb38df9881a5340d09eff7cb27e2286
ritatsousa <53748834+ritatsousa@users.noreply.github.com>
2023-01-23 08:14:21 +0000

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,34 +1,50 @@
-#number of dimensions that the generated embeddings have
-EMBEDDING_SIZE = 200
+#number of dimensions of the embeddings
+EMBEDDING_SIZE = 100
+
 #maximum number of walks per entity
 N_WALKS = 100
+
 #maximum depth of walks
 WALK_DEPTH = 4
+
 #options are "word2vec" e "wang2vec" (order-aware)
 LANGUAGE_MODEL = "word2vec"
-#folder where the embeddings, walks and models files will be stored
+
+#folder where the embeddings, walks and models files will be saved
 PATH_OUTPUT_EMBEDDING = "Embeddings/PPI/"
-#path file where we have one entity per line
+
+#path file with one entity per line
 PATH_ENTITY_FILE = "Data/PPI/Prots.txt"
-#owl ontology file
+
+#ontology file path in the OWL file
 ONTOLOGY_FILE_PATH = "Data/GO/go.owl"
-#file path in tsv format with one protein per line and with '+' and '-' to indicate if it is a positive or negative annotation
+
+#annotations file path in the tsv format with one protein per line and with '+' and '-' to indicate if it is a positive or negative annotation
 ANNOTATIONS_FILE_PATH = "Data/GO/GO_annotations.tsv"
 
 
-# dataset file path. The format of each line of the dataset files is "Ent1\tEnt2\tLabel\n" 
+##### Evaluation 
+
+# dataset file path, where each line of the dataset files is in the format "Ent1\tEnt2\tLabel\n" 
 FILE_DATASET = "Data/PPI/pairs_PPIdataset.txt"
-#list of the embeddings files (positive and negative)
+
+#list of the embeddings files (one file for the embeddings generated with positive statements and the other for the embeddings generated with negative statements)
 EMBEDDING_FILES = ["Embeddings/PPI/Emb100_TrueWalks_pos.txt", "Embeddings/PPI/Emb100_TrueWalks_neg.txt"]
-#operator that will be used to combine the embeddings of the pair
+
+#operator to combine the embeddings of a pair
 OPERATION = "hada"
-#folder where predictions and metrics are stored
+
+#folder where predictions and metrics are saved
 PATH_OUTPUT_ML = "Results/PPI-ML"
-#folder where the split indexes are stored
+
+#folder where the MCCV split indexes are saved
 PATH_PARTITION_ML = "Results/PPI-ML/IndexTest_MCCV_Run"
-#number of splits
+
+#number of splits for MCCV
 N_PARTITIONS = 30
-#fraction of the dataset that will correspond to the test set
+
+#fraction of the dataset that will correspond to the test set in the MCCV
 TEST_SIZE = 0.3
-#folder where similarity metrics are stored
+
+#folder where similarity metrics are saved
 PATH_OUTPUT_SS = "Results/PPI-sim/"
